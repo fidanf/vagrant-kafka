a96cc173eb32fad11252a70901ff6d21d71f97bf
Frank Fidanza <frankfid@wanadoo.fr>
2019-06-05 12:36:53 +0200

    Fixed version conflict

--- scripts/list-consumer-groups.sh
+++ scripts/list-consumer-groups.sh
@@ -1,3 +1,8 @@
-#!/bin/bash -eux 
+#!/bin/bash
+
+if [ $KAFKA_VERSION == '0.10.0.1' ] ; then 
+    $KAFKA_HOME/bin/kafka-consumer-groups.sh --zookeeper $ZK_CLUSTER --list
+    exit 0
+fi
 
 $KAFKA_HOME/bin/kafka-consumer-groups.sh --bootstrap-server $KAFKA_CLUSTER --list
