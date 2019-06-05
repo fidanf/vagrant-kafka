aeda760db00d32f20ab26b77e8024f56032d673b
Frank Fidanza <frankfid@wanadoo.fr>
2019-06-05 12:36:46 +0200

    Deleting snapshots from zk import

--- scripts/import-zk.sh
+++ scripts/import-zk.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
+#!/bin/bash -eux
 
 tar -C /tmp/zookeeper -xzf $KAFKA_TARGET/zookeeper-backup.tar.gz --strip-components=2
 pushd /tmp/zookeeper/
 mv zookeeper/version-2/* version-2/
 rm -rf zookeeper/
 popd
+
+# delete snapshots
+echo 'deleting snapshots'
+ls /tmp/zookeeper/version-2/snapshot.* | xargs rm -f
