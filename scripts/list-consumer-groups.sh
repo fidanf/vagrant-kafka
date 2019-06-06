45df163796d6b34db75b4ca933e8e96c82dce9e4
Frank Fidanza <frankfid@wanadoo.fr>
2019-06-06 13:58:41 +0200

    Added commented alternative comment

--- scripts/list-consumer-groups.sh
+++ scripts/list-consumer-groups.sh
@@ -6,3 +6,4 @@ if [ $KAFKA_VERSION == '0.10.0.1' ] ; then
 fi
 
 $KAFKA_HOME/bin/kafka-consumer-groups.sh --bootstrap-server $KAFKA_CLUSTER --list
+# $KAFKA_HOME/bin/kafka-consumer-groups.sh --bootstrap-server "1,2,3"--list
