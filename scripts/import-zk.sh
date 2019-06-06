7afed618a75e194095919fe24c2e897abe969d27
Frank Fidanza <frankfid@wanadoo.fr>
2019-06-06 15:00:42 +0200

    Refactoring import zk script

--- scripts/import-zk.sh
+++ scripts/import-zk.sh
@@ -3,16 +3,11 @@
 # create myid file. see http://zookeeper.apache.org/doc/r3.1.1/zookeeperAdmin.html#sc_zkMulitServerSetup
 if [ ! -d /tmp/zookeeper ]; then
     echo creating zookeeper data dir...
-    mkdir /tmp/zookeeper
+    mkdir -p /tmp/zookeeper
     echo $1 > /tmp/zookeeper/myid
 fi
 
-tar -C /tmp/zookeeper -xzf $KAFKA_TARGET/zookeeper-backup.tar.gz --strip-components=2
-pushd /tmp/zookeeper/
-mkdir -p version-2/
-mv zookeeper/version-2/* version-2/
-rm -rf zookeeper/
-popd
+tar -C /tmp/zookeeper -xzf $KAFKA_TARGET/zookeeper-backup.tar.gz --strip-components=3 version-2
 
 # delete snapshots
 # echo 'deleting snapshots'
