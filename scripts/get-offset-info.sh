552a3ebc2969dcc484b5b98648d90b21157d6831
Frank Fidanza <fidanza.frank@gmail.com>
2019-10-29 10:07:41 +0100

    Add header before stdout

--- scripts/get-offset-info.sh
+++ scripts/get-offset-info.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [ $# -gt 0 ]; then
-   $KAFKA_HOME/bin/kafka-run-class.sh kafka.tools.GetOffsetShell --broker-list $KAFKA_CLUSTER --topic $1 --time -1
+    echo "TOPIC NAME | PARTITION ID | LATEST OFFSET"
+    $KAFKA_HOME/bin/kafka-run-class.sh kafka.tools.GetOffsetShell --broker-list $KAFKA_CLUSTER --topic $1 --time -1
 else
     echo "Usage: "$(basename $0)" <topic_name>"
 fi
